68bb46b254333b34b6321beabb1c01aa299c08c6
Cheah Seong Teng <tengstc@gmail.com>
2024-04-22 14:13:30 +0800

    NEW

--- src/pages/AboutusPage.jsx
+++ src/pages/AboutusPage.jsx
@@ -145,7 +145,7 @@ export default function AboutPage() {
           </Row>
         </Container>
       </section>
-      <p className="author"><span>© 2024 </span>Cheah Seong Teng</p>
+      <p className="author"><span>© 2024 CYBER5H13LD</span><br></br>Cheah Seong Teng</p>
     </>
   );
 }
